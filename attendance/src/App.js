73d91b89f3a6eff8c8d64a7a22dbd44a2d948ffd
skimmy44 <jjswhappy@gmail.com>
2022-01-20 17:24:03 -0800

    20/01/2022 Frontend

--- attendance/src/App.js
+++ attendance/src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, Form, Card } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card className="mb-3" style={{ color: "#000", width: '25rem', height: '20rem'}}>
+          <Card.Title>CPSC 213 Attendance</Card.Title>
+        <Form>
+          <Form.Group>
+            <Form.Label>Student Number</Form.Label>
+            <Form.Control type="email" placeholder="Enter the 8 digits of your Student Number" />
+          </Form.Group>
+          <Form.Group controlID="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          
+          <Button variant="primary" type="submit">Sumbit</Button>
+        </Form>
+        </Card>
       </header>
     </div>
   );
